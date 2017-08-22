354b6fe9ddc9bd0d558cbc48cd367d2afb2868fa
Volodymyr <vivk2@ukr.net>
2017-08-22 17:43:35 +0300

    Java Varrargs

---
+++ src/JavaVarargs.java
@@ -0,0 +1,12 @@
+
+public class JavaVarargs {
+		void add(Integer... a){
+				int b=0;
+				for(int i=0;i<a.length;i++){
+					System.out.print(a[i]);
+					System.out.print(i==a.length-1 ? "=" : "+");
+					b+=a[i];
+				}
+				System.out.println(b);
+			}
+		}
